d2974ebbb797258c40a0f2ebcf9d310bed07aa69
KMJ_DEV <dds13462@gmail.com>
2023-04-18 17:19:30 +0900

    환경생성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>지키고 싶은 나만의 루틴</h1>
+    <img class="rabbit" src="/images/rabbit.png" alt="토끼로고">
+    <a href="/components/"></a>
+    <Todo />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from "./components/TodoList.vue"
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Todo,
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  max-width: 700px;
+  margin: 0 auto;
+  background: rgb(245, 245, 245);
+}
+
+.rabbit {
+  width: 50%;
+  display: block;
+  margin: 0 auto;
 }
 </style>
